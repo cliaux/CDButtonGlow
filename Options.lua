799045b068cd681f7a2ee3ff59ffa3b06e33ce38
Cli(aux) <you@example.com>
2026-03-12 23:14:06 +0100

    Fix addon UI refresh logic using IsOpen method instead of checking OpenFrames directly

--- Options.lua
+++ Options.lua
@@ -75,7 +75,7 @@ local function UpdateExcludedSpellsOptionsCallback(self)
     
     -- Refresh the options dialog if it's open
     local dialog = LibStub('AceConfigDialog-3.0')
-    if dialog.OpenFrames and dialog.OpenFrames[AddonName] then
+    if dialog:IsOpen(AddonName) then
         dialog:SelectGroup(AddonName, "excludedSpells")
         dialog:SelectGroup(AddonName, "general") -- Reset to general tab
         dialog:SelectGroup(AddonName, "excludedSpells") -- Back to excluded spells
